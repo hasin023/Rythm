e0bf18af0c7b3425cdbbb48ff562197c58153e27
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-06-18 23:15:04 +0600

    Playback bar middle section

--- index.php
+++ index.php
@@ -20,10 +20,83 @@ if(isset($_SESSION['userLoggedIn'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta
+      name="description"
+      content="Rythm is a free music streaming service that allows the user to listen to their favourite tunes for free."
+    />
     <link rel = "icon shortcut" href = "img/music.ico">
+    <link rel="stylesheet" type="text/css" href = "css/index.css">
     <title>Rythm | Music you love</title>
 </head>
 <body>
-    <h1>Goodbye World!!</h1>
+    
+
+    <div id = "nowPlayingBarContainer">
+
+        <div id = "nowPlayingBar">
+
+            <div id = "nowPlayingLeft">
+
+            </div>
+
+            <div id = "nowPlayingCenter">
+                <div class = "content playerControls">
+                    <div class = "buttons">
+                        <button class = "btn shuffle" title = "Shuffle button">
+                        <ion-icon name="shuffle"></ion-icon>
+                        </button>
+
+                        <button class = "btn back" title = "Back button">
+                        <ion-icon name="play-skip-back-sharp"></ion-icon>
+                        </button>
+
+                        <button class = "btn play" title = "Play button">
+                        <ion-icon class = "play" name="play-circle-sharp"></ion-icon>
+                        </button>
+
+                        <button class = "btn pause" title = "Pause button" style = "display: none">
+                        <ion-icon class = "pause" name="pause-circle"></ion-icon>
+                        </button>
+
+                        <button class = "btn forward" title = "Forward button">
+                        <ion-icon name="play-skip-forward-sharp"></ion-icon>
+                        </button>
+
+                        <button class = "btn repeat" title = "Repeat button">
+                        <ion-icon name="repeat"></ion-icon>
+                        </button>
+                    </div>
+
+                    <div class = "playBackBar">
+                        <span class = "progressTime current">0:00</span>
+                        <div class = "progressBar">
+                            <div class = "progressBarBg">
+                                <div class = "progress">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                        <span class = "progressTime remaining">0:00</span>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div id = "nowPlayingRight">
+
+            </div>
+
+
+
+        </div>
+
+    </div>
+
+
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
