3b5d48b2d3cbd866850bf01d0c9d26e42e24a677
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-06-19 02:15:56 +0600

    Complete music playback design

--- index.php
+++ index.php
@@ -36,6 +36,21 @@ if(isset($_SESSION['userLoggedIn'])) {
         <div id = "nowPlayingBar">
 
             <div id = "nowPlayingLeft">
+                <div class = "content">
+                    <span class = "albumLink">
+                        <img class = "albumArtwork" src = "img/spiderverseCover.jpg">
+                    </span>
+
+                    <div class = "trackInfo">
+                        <span class = "trackName">
+                            <span>Mona Lisa</span>
+                        </span>
+
+                        <span class = "artistName">
+                            <span>Dominic Fike</span>
+                        </span>
+                    </div>
+                </div>
 
             </div>
 
@@ -72,7 +87,7 @@ if(isset($_SESSION['userLoggedIn'])) {
                         <div class = "progressBar">
                             <div class = "progressBarBg">
                                 <div class = "progress">
-                                    
+
                                 </div>
                             </div>
                         </div>
@@ -85,6 +100,27 @@ if(isset($_SESSION['userLoggedIn'])) {
             </div>
 
             <div id = "nowPlayingRight">
+                <div class = "volumeBar">
+                    <button class = "btn volume-high" title = "Volume button">
+                    <ion-icon class = "volume-high" name="volume-high-outline"></ion-icon>
+                    </button>
+
+                    <button class = "btn volume-medium" title = "Medium button" style = "display: none">
+                    <ion-icon class = "volume-medium" name="volume-medium-outline"></ion-icon>
+                    </button>
+
+                    <button class = "btn volume-mute" title = "Mute button" style = "display: none">
+                    <ion-icon class = "volume-mute" name="volume-mute-outline"></ion-icon>
+                    </button>
+
+                    <div class = "progressBar">
+                            <div class = "progressBarBg">
+                                <div class = "progress">
+
+                                </div>
+                            </div>
+                        </div>
+                </div>
 
             </div>
 
