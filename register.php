62fa3a30ff530ab6f0e62ddfa7356798f66ec92d
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-06-18 15:33:06 +0600

    Added register css style

--- register.php
+++ register.php
@@ -25,63 +25,73 @@ function getInput($name){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel = "icon shortcut" href = "img/music.ico">
     <title>Register</title>
+    <link rel = "stylesheet" type = "text/css" href = "css/register.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&family=Poppins:wght@200;300;400&display=swap" rel="stylesheet">
+
 </head>
 <body>
-    <div id = "inputContainer">
-        <form id = "loginForm" action = "register.php" method = "POST">
-            <h3>Log in to your Rythm account</h3>
-            <p>
-                <?php echo $account->getError(Constants::$loginFailed); ?>
-                <label for = "loginUsername">Username</label>
-                <input id = "loginUsername" name = "loginUsername" type = "text" placeholder = "Username" required>
-            </p>
-            <p>
-                <label for = "loginPassword">Password</label>
-                <input id = "loginPassword" name = "loginPassword" type = "password" placeholder = "Password" required>
-            </p>
-            <button type = "submit" name = "loginButton">LOG IN</button>
-        </form>
-
-
-
-        <form id = "registrationForm" action = "register.php" method = "POST">
-            <h3>Create your free account today!!</h3>
-            <p>
-                <?php echo $account->getError(Constants::$usernameCharacters); ?>
-                <?php echo $account->getError(Constants::$usernameTaken); ?>
-                <label for = "username">Username</label>
-                <input id = "username" name = "username" type = "text" placeholder = "Username" value = "<?php getInput('username') ?>" required>
-            </p>
-            <p>
-                <?php echo $account->getError(Constants::$nameCharacters); ?>
-                <label for = "firstName">First Name</label>
-                <input id = "firstName" name = "firstName" type = "text" placeholder = "First Name" value = "<?php getInput('firstName') ?>" required>
-            </p>
-            <p>
-                <?php echo $account->getError(Constants::$nameCharacters); ?>
-                <label for = "lastName">Last Name</label>
-                <input id = "lastName" name = "lastName" type = "text" placeholder = "Last Name" value = "<?php getInput('lastName') ?>" required>
-            </p>
-            <p>
-                <?php echo $account->getError(Constants::$emailInvalid); ?>
-                <?php echo $account->getError(Constants::$emailTaken); ?>
-                <label for = "email">Email</label>
-                <input id = "email" name = "email" type = "email" placeholder = "Email" value = "<?php getInput('email') ?>" required>
-            </p>
-            <p>
-                <?php echo $account->getError(Constants::$passwordCharacters); ?>
-                <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
-                <label for = "password">Password</label>
-                <input id = "password" name = "password" type = "password" placeholder = "Password" required>
-            </p>
-            <p>
-                <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
-                <label for = "confirmPassword">Re-enter Password</label>
-                <input id = "confirmPassword" name = "confirmPassword" type = "password" placeholder = "Confirm password" required>
-            </p>
-            <button type = "submit" name = "registerButton">SIGN UP</button>
-        </form>
-
+    <div class= "backgroundImg">
+        <div class = "loginContainer">
+            <div class = "inputContainer">
+                <form id = "loginForm" action = "register.php" method = "POST">
+                    <h3>Log into your <span class = "highlight">Rythm</span> account</h3>
+                    <p>
+                        <?php echo $account->getError(Constants::$loginFailed); ?>
+                        <label class = "inputLabel" for = "loginUsername">Username</label>
+                        <input class = "textBox" id = "loginUsername" name = "loginUsername" type = "text" placeholder = "Username" required>
+                    </p>
+                    <p>
+                        <label class = "inputLabel" for = "loginPassword">Password</label>
+                        <input class = "textBox" id = "loginPassword" name = "loginPassword" type = "password" placeholder = "Password" required>
+                    </p>
+                    <button class = "submitButton" type = "submit" name = "loginButton">LOG IN</button>
+                </form>
+                
+                
+                
+                <form id = "registrationForm" action = "register.php" method = "POST">
+                    <h3>Create your free account today!!</h3>
+                    <p>
+                        <?php echo $account->getError(Constants::$usernameCharacters); ?>
+                        <?php echo $account->getError(Constants::$usernameTaken); ?>
+                        <label class = "inputLabel" for = "username">Username</label>
+                        <input class = "textBox" id = "username" name = "username" type = "text" placeholder = "Username" value = "<?php getInput('username') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$nameCharacters); ?>
+                        <label class = "inputLabel" for = "firstName">First Name</label>
+                        <input class = "textBox" id = "firstName" name = "firstName" type = "text" placeholder = "First Name" value = "<?php getInput('firstName') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$nameCharacters); ?>
+                        <label class = "inputLabel" for = "lastName">Last Name</label>
+                        <input class = "textBox" id = "lastName" name = "lastName" type = "text" placeholder = "Last Name" value = "<?php getInput('lastName') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$emailInvalid); ?>
+                        <?php echo $account->getError(Constants::$emailTaken); ?>
+                        <label class = "inputLabel" for = "email">Email</label>
+                        <input class = "textBox" id = "email" name = "email" type = "email" placeholder = "Email" value = "<?php getInput('email') ?>" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$passwordCharacters); ?>
+                        <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+                        <label class = "inputLabel" for = "password">Password</label>
+                        <input class = "textBox" id = "password" name = "password" type = "password" placeholder = "Password" required>
+                    </p>
+                    <p>
+                        <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+                        <label class = "inputLabel" for = "confirmPassword">Re-enter Password</label>
+                        <input class = "textBox" id = "confirmPassword" name = "confirmPassword" type = "password" placeholder = "Confirm password" required>
+                    </p>
+                    <button class = "submitButton" type = "submit" name = "registerButton">SIGN UP</button>
+                </form>
+        
+            </div>
+        </div>
     </div>
 </body>
 </html>
