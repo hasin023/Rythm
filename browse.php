0bfaf286a3ca9545f8eb691444aa4b8dec401869
Hasin Mahtab <hasinmahtab.alvee@gmail.com>
2023-06-24 15:07:47 +0600

    Added browse page

---
+++ browse.php
@@ -0,0 +1,31 @@
+<?php
+include("includes/includedFiles.php");
+?>
+
+
+<h1  class="pageHeadingBig">Browse Page</h1>
+
+<div class = "gridViewContainer">
+
+    <?php
+
+    $albumQuery = mysqli_query($con, "SELECT * FROM albums ORDER BY RAND() LIMIT 10");
+
+    while ($row = mysqli_fetch_array($albumQuery)) {
+
+        echo "<span class = 'cardLink' role = 'link' tabindex = '0' onclick = 'openPage(\"album.php?id=" . $row['id'] . "\")'> 
+                    <div class='card'>" .
+            "<div class='gridViewItem'>
+                            <img src='" . $row['artworkPath'] . "'>
+
+                            <div class='gridViewInfo'>"
+            . $row['title'] .
+            "</div>
+                        </div>
+                    </div>
+                </span>";
+    }
+
+    ?>
+
+</div>
